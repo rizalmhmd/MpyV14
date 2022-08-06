6d42fd8a2978e0cd3404eacaca0151eb09e066af
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-06 18:05:13 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,19 +1,15 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
+let res = await fetch(`https://api.xteam.xyz/dl/tiktok?url=${args[0]}&APIKEY=9db5a5373433c015`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-let { description, author, statistic, link } = json.result
-await conn.sendFile(m.chat, link, 'tt.mp4', `
-▶ ${statistic.playCount} Views
-❤ ${statistic.diggCount} Likes
-🔁 ${statistic.shareCount} Shares
-💬 ${statistic.commentCount} Comments
-- *By:* ${author.nickname} (${author.username})
-- *Desc:*
-${description}
+let { caption, name, thumb_img, link_dl1, } = json.result
+await conn.sendFile(m.chat, link_dl1, 'tt.mp4', `
+𝙉𝙖𝙢𝙖 : *${name}*
+𝘾𝙖𝙥𝙩𝙞𝙤𝙣 : *${caption}*
+𝙇𝙞𝙣𝙠 : ${link_dl1}
 `.trim(), m)
 }
 
