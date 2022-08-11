879b286420e500f6e1c2a5bae1cfaa785b2ae5c4
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-11 12:55:53 +0700

    Update tiktokmp3.js

--- plugins/tiktokmp3.js
+++ plugins/tiktokmp3.js
@@ -1,15 +1,15 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktokmusic?apikey=${lolkey}&url=${args[0]}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-await conn.sendFile(m.chat, url, 'tt.audio', m)
+if (!args[0]) throw 'url nya mana ngenn?'
+let res = await fetch(`http://zekais-api.herokuapp.com/tiktokmusic?url=${args[0]}&apikey=zekais`)
+ if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.result, 'tt.audio' m)
 }
 
 handler.help = ['tiktokmp3 <url>']
 handler.tags = ['downloader']
 handler.command = /^tiktokmp3$/i
+
 
 module.exports = handler
