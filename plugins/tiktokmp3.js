d2c9541f62711ff075f2974ecbcaa64896e4f337
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-11 12:56:15 +0700

    Update tiktokmp3.js

--- plugins/tiktokmp3.js
+++ plugins/tiktokmp3.js
@@ -4,7 +4,7 @@ if (!args[0]) throw 'url nya mana ngenn?'
 let res = await fetch(`http://zekais-api.herokuapp.com/tiktokmusic?url=${args[0]}&apikey=zekais`)
  if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.result, 'tt.audio' m)
+    await conn.sendFile(m.chat, json.result, 'tt.audio', m)
 }
 
 handler.help = ['tiktokmp3 <url>']
